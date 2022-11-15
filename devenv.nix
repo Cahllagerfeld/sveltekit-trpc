d427044712f423b3303181fcaf0ebeba317d1621
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-11-15 16:44:04 +0000

    fix

--- devenv.nix
+++ devenv.nix
@@ -1,16 +1,18 @@
 { pkgs, ... }:
 
 {
-  # https://devenv.sh/basics/
-  env.GREET = "devenv";
-
   # https://devenv.sh/packages/
   packages = [ pkgs.git pkgs.nodejs-16_x ];
 
 
   enterShell = ''
     npm install
+    npm i -g pnpm
   '';
+
+  processes = {
+    dev.exec = "npm run dev";
+  };
 
   # https://devenv.sh/languages/
   languages.nix.enable = true;
